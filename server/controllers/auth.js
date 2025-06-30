41c79875ecf9cf63094226fa1547ff4d002005c8
Marmik Vyas <161038103+marmikvyass@users.noreply.github.com>
2025-07-01 01:21:21 +0530

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -30,8 +30,8 @@ export const register = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure:true,
+            sameSite:'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         //sending welcome email to user
@@ -73,8 +73,8 @@ export const userLogin = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite:'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
