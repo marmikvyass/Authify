f5e1ae8ea7123e34e5fedecb3653ecf11c711b4d
Marmik Vyas <161038103+marmikvyass@users.noreply.github.com>
2025-06-30 22:43:25 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,16 +5,19 @@ import { toast } from "react-toastify";
 export const AppContext = createContext()
 
 export const AppContextProvider = (props)=>{
-
+    
+    
+    const backendURL = import.meta.env.VITE_BACKEND_URL
+    axios.defaults.baseURL = backendURL;
     axios.defaults.withCredentials = true
 
-    const backendURL = import.meta.env.VITE_BACKEND_URL
+    
     const [isLoggedin, setIsLoggedin] = useState(false)
     const [userData, setUserData] = useState(false)
 
     const getAuthStatus = async ()=>{
         try{
-            const {data} = await axios.get(backendURL + '/api/auth/isAuth')
+            const {data} = await axios.get('/api/auth/isAuth')
             if(data.success){
                 setIsLoggedin(true)
                 getUserData()
@@ -26,7 +29,7 @@ export const AppContextProvider = (props)=>{
 
     const getUserData = async ()=>{
         try{
-            const {data} = await axios.get(backendURL + '/api/user/data')
+            const {data} = await axios.get('/api/user/data')
             data.success ? setUserData(data.userData) : toast.error(data.message)
         }catch(error){
             toast.error(error.message)
