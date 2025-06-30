aeb03cf6ca7c3274a0ac4f0a4dffdea99e068294
Marmik Vyas <161038103+marmikvyass@users.noreply.github.com>
2025-06-30 23:20:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,11 +11,22 @@ const app = express()
 const port = process.env.PORT || 3000
 connectDB()
 
-const allowedOrigins = ['https://authify-i5bh.onrender.com']
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://authify-flax.vercel.app'
+]
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins ,credentials:true}));
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps, Postman)
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true
+}))
 //API endpoints
 app.get('/', (req, res)=>{
     res.send('API WORKING')
